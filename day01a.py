b978c3a67dbd5df3d4650eec7014e0eac4811ac7
Marek Pšenka <psenka@edhouse.cz>
2025-12-02 07:18:51 +0100

    Day 01

--- day01a.py
+++ day01a.py
@@ -2,5 +2,17 @@ import fileinput
 
 input = map(str.rstrip, fileinput.input())
 
+pos = 50
+zero_count = 0
 for line in input:
-    print(line)
+    if line[0] == "L":
+        pos = pos - int(line[1:]) % 100
+    elif line[0] == "R":
+        pos = (pos + int(line[1:])) % 100
+    else:
+        raise RuntimeError
+
+    if pos == 0:
+        zero_count += 1
+
+print(zero_count)
--- day01b.py
+++ day01b.py
@@ -2,5 +2,46 @@ import fileinput
 
 input = map(str.rstrip, fileinput.input())
 
+
+def rotate(pos: int, rot_str: str) -> tuple[int, int]:
+    zero_count = 0
+    rotation = int(rot_str[1:])
+    if rot_str[0] == "L":
+        new_pos = pos - rotation
+        if new_pos <= 0:
+            if pos == 0:
+                zero_count += rotation // 100
+            else:
+                zero_count += 1 + (-new_pos) // 100
+
+    elif rot_str[0] == "R":
+        new_pos = pos + rotation
+        if new_pos >= 100:
+            zero_count += new_pos // 100
+    else:
+        raise RuntimeError
+
+    return new_pos % 100, zero_count
+
+
+pos = 50
+total_zero_count = 0
 for line in input:
-    print(line)
+    pos, clicks = rotate(pos, line)
+    total_zero_count += clicks
+
+print(total_zero_count)
+
+assert rotate(50, "R1000") == (50, 10)
+assert rotate(50, "L100") == (50, 1)
+assert rotate(50, "L1000") == (50, 10)
+assert rotate(0, "L100") == (0, 1)
+assert rotate(0, "L1") == (99, 0)
+assert rotate(3, "L4") == (99, 1)
+assert rotate(99, "R1") == (0, 1)
+assert rotate(99, "R100") == (99, 1)
+assert rotate(0, "R100") == (0, 1)
+assert rotate(0, "L99") == (1, 0)
+assert rotate(50, "L50") == (0, 1)
+assert rotate(50, "R50") == (0, 1)
+assert rotate(0, "L101") == (99, 1)
