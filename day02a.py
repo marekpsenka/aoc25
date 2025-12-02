7d1db9869deb12fb7c6fd089e1a97c87b58e45e3
Marek Pšenka <psenka@edhouse.cz>
2025-12-02 18:03:29 +0100

    Day 02

--- day02a.py
+++ day02a.py
@@ -2,5 +2,22 @@ import fileinput
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+total = 0
+for range_str in next(input).split(","):
+    sub_total = 0
+    left_str, right_str = range_str.split("-")
+    left, right = int(left_str), int(right_str)
+    if len(left_str) == 1:
+        pali_half = 1
+    else:
+        pali_half = int(left_str[: (len(left_str) // 2)])
+    while True:
+        pali = max(int(str(pali_half) + str(pali_half)), 1)
+        if pali > right:
+            break
+        pali_half += 1
+        if pali >= left:
+            sub_total += pali
+    total += sub_total
+
+print(total)
--- day02b.py
+++ day02b.py
@@ -1,6 +1,36 @@
 import fileinput
+from dataclasses import dataclass
+
+
+@dataclass
+class Range:
+    left: int
+    right: int
+
+    def contains(self, n: int) -> bool:
+        return n >= self.left and n <= self.right
+
 
 input = map(str.rstrip, fileinput.input())
+ranges: list[Range] = []
+for range_str in next(input).split(","):
+    left_str, right_str = range_str.split("-")
+    ranges.append(Range(int(left_str), int(right_str)))
 
-for line in input:
-    print(line)
+ranges.sort(key=lambda range: range.left)
+
+total = 0
+found: set[int] = set()
+for seed in range(1, 100000):
+    id_str = 2 * str(seed)
+    id = int(id_str)
+    while id < 10000000000:
+        for range in ranges:
+            if range.contains(id) and id not in found:
+                total += id
+                found.add(id)
+                break
+        id_str += str(seed)
+        id = int(id_str)
+
+print(sum(found))
