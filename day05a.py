40ae964d6a5f0148f8d1e85125b8e58b9c700e15
Marek Pšenka <psenka@edhouse.cz>
2025-12-05 20:12:12 +0100

    Day 05

--- day05a.py
+++ day05a.py
@@ -1,6 +1,28 @@
 import fileinput
+from dataclasses import dataclass
+from itertools import takewhile
+
+
+@dataclass
+class Range:
+    left: int
+    right: int
+
+    def contains(self, n: int) -> bool:
+        return n >= self.left and n <= self.right
+
 
 input = map(str.rstrip, fileinput.input())
 
+range_strs = takewhile(lambda s: s, input)
+ranges: list[Range] = list()
+for range_str in range_strs:
+    left_str, right_str = range_str.split("-")
+    ranges.append(Range(int(left_str), int(right_str)))
+
+fresh = 0
 for line in input:
-    print(line)
+    if any(map(lambda range: range.contains(int(line)), ranges)):
+        fresh += 1
+
+print(fresh)
--- day05b.py
+++ day05b.py
@@ -1,6 +1,41 @@
 import fileinput
+from dataclasses import dataclass
+from itertools import takewhile
+
+
+@dataclass
+class Range:
+    left: int
+    right: int
+
+    def contains(self, n: int) -> bool:
+        return n >= self.left and n <= self.right
+
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+range_strs = takewhile(lambda s: s, input)
+ranges: list[Range] = list()
+for range_str in range_strs:
+    left_str, right_str = range_str.split("-")
+    ranges.append(Range(int(left_str), int(right_str)))
+
+ranges.sort(key=lambda range: range.left)
+
+i = 0
+total = 0
+while i < len(ranges):
+    j = i + 1
+    maximal = ranges[i].right
+    while j < len(ranges):
+        if ranges[j].left <= maximal:
+            if maximal < ranges[j].right:
+                maximal = ranges[j].right
+            j += 1
+        else:
+            break
+    total += maximal - ranges[i].left + 1
+    i = j
+
+
+print(total)
