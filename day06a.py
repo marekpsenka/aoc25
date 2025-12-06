19aac02305452e117648e5029204d54d06828120
Marek Pšenka <psenka@edhouse.cz>
2025-12-06 06:46:42 +0100

    Day 06

--- day06a.py
+++ day06a.py
@@ -1,6 +1,22 @@
 import fileinput
+from math import prod
 
-input = map(str.rstrip, fileinput.input())
+input = list(fileinput.input())
 
-for line in input:
-    print(line)
+op_loc = list(filter(lambda p: p[1] != " ", enumerate(input[-1])))
+
+total = 0
+for i in range(len(op_loc)):
+    left, op = op_loc[i]
+    if i == len(op_loc) - 1:
+        right = len(input[0]) - 1
+    else:
+        right = op_loc[i + 1][0] - 1
+
+    numbers = [int(input[j][left:right]) for j in range(len(input) - 1)]
+    if op == "+":
+        total += sum(numbers)
+    else:
+        total += prod(numbers)
+
+print(total)
