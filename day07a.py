f3346e3ed26952e9c43cdfadc2e525542c3746c6
Marek Pšenka <psenka@edhouse.cz>
2025-12-07 18:51:34 +0100

    Day 07

--- day07a.py
+++ day07a.py
@@ -1,6 +1,41 @@
 import fileinput
+from collections import defaultdict
 
-input = map(str.rstrip, fileinput.input())
+Point = tuple[int, int]
 
-for line in input:
-    print(line)
+
+def first_larger(ss: list[int], i: int) -> int | None:
+    for j in ss:
+        if j > i:
+            return j
+    return None
+
+
+input = list(map(str.rstrip, fileinput.input()))
+splitters: defaultdict[int, list[int]] = defaultdict(list)
+queue: list[Point] = []
+
+for i, line in enumerate(input):
+    for j, c in enumerate(line):
+        if c == "S":
+            queue.append((i, j))
+        elif c == "^":
+            splitters[j].append(i)
+
+hit: set[Point] = set()
+
+while len(queue) > 0:
+    o_i, o_j = queue.pop()
+    match first_larger(splitters[o_j], o_i):
+        case None:
+            continue
+        case int() as i:
+            if (i, o_j) in hit:
+                continue
+            if o_j < len(input[0]) - 1:
+                queue.append((i, o_j + 1))
+            if o_j > 1:
+                queue.append((i, o_j - 1))
+            hit.add((i, o_j))
+
+print(len(hit))
--- day07b.py
+++ day07b.py
@@ -1,6 +1,51 @@
 import fileinput
+from collections import defaultdict
 
-input = map(str.rstrip, fileinput.input())
+Point = tuple[int, int]
 
-for line in input:
-    print(line)
+
+def first_larger(ss: list[int], i: int) -> int | None:
+    for j in ss:
+        if j > i:
+            return j
+    return None
+
+
+input = list(map(str.rstrip, fileinput.input()))
+splitters: defaultdict[int, list[int]] = defaultdict(list)
+multiplicity: dict[Point, int] = dict()
+start: Point | None = None
+
+for i, line in enumerate(input):
+    for j, c in enumerate(line):
+        if c == "S":
+            start = (i, j)
+        elif c == "^":
+            splitters[j].append(i)
+            multiplicity[(i, j)] = 0
+
+assert start is not None
+first = first_larger(splitters[start[1]], start[0])
+assert first is not None
+multiplicity[(first, start[1])] += 1
+
+total = 0
+
+for s_i, s_j in multiplicity.keys():
+    mult = multiplicity[(s_i, s_j)]
+    if mult == 0:
+        continue
+    if s_j < len(input[0]) - 1:
+        match first_larger(splitters[s_j + 1], s_i):
+            case None:
+                total += mult
+            case int() as i:
+                multiplicity[(i, s_j + 1)] += mult
+    if s_j > 0:
+        match first_larger(splitters[s_j - 1], s_i):
+            case None:
+                total += mult
+            case int() as i:
+                multiplicity[(i, s_j - 1)] += mult
+
+print(total)
